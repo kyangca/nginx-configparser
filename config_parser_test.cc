ebf6a1160b8efd9d5305314027d6857674970d98
Kevin Yang <kyang3@caltech.edu>
2014-04-18 00:02:07 -0700

    Made Unit Tests For Nginx Config Parser
    Created various unit tests for an Nginx config
       parser.  The configs needed for these tests
       have also been included.  Some of these
       tests fail due to bugs in the parser.

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -9,3 +9,43 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+TEST(NginxConfigParserTest, NestedEndBlock) {
+    NginxConfigParser parser;
+    NginxConfig out_config;
+
+    bool success = parser.Parse("nested_config", &out_config);
+    EXPECT_TRUE(success);   
+}
+
+TEST(NginxConfigParserTest, FullNginxConfig) {
+    NginxConfigParser parser;
+    NginxConfig out_config;
+
+    bool success = parser.Parse("full_nginx_config", &out_config);
+    EXPECT_TRUE(success);
+}
+
+TEST(NginxConfigParserTest, DoubleEndStatement) {
+    NginxConfigParser parser;
+    NginxConfig out_config;
+
+    bool success = parser.Parse("double_end_config", &out_config);
+    EXPECT_FALSE(success);
+}
+
+TEST(NginxConfigParserTest, NoEndStatementBeforeEOF) {
+    NginxConfigParser parser;
+    NginxConfig out_config;
+
+    bool success = parser.Parse("no_end_config", &out_config);
+    EXPECT_FALSE(success);
+}
+
+TEST(NginxConfigParserTest, CommentInToken) {
+    NginxConfigParser parser;
+    NginxConfig out_config;
+
+    bool success = parser.Parse("hashtag_config", &out_config);
+    EXPECT_FALSE(success);
+}
